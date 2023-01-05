bc2505bd41d8315c71f2c83a7a567ce8f1290e60
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-01-04 22:52:21 -0500

    Add GameManager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using Cinemachine;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Instance
+    public static GameManager GM;
+
+    // Variables
+    [HideInInspector] public bool gameIsRunning = true;
+
+    // Runs before a scene gets loaded
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void LoadGM()
+    {
+        GameObject GM = Instantiate(Resources.Load("Prefabs/GameManager")) as GameObject;
+        DontDestroyOnLoad(GM);
+    }
+
+    private void Awake()
+    {
+        //Application.targetFrameRate = 30;
+
+        if (GM == null)
+        {
+            GM = this;
+        }
+        else if (GM != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        // It is save to remove listeners even if they
+        // didn't exist so far.
+        // This makes sure it is added only once
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        // Add the listener to be called when a scene is loaded
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+    }
+}
