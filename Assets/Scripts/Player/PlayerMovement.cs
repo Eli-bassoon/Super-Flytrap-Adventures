ebb3a839ad08f45d6b3cafc22588ba30534a7800
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-03-03 00:25:40 -0500

    Change from foldouts to headers

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,43 +15,29 @@ public class PlayerMovement : MonoBehaviour
     public static PlayerMovement instance;
     public static int mouthStateAnim = Animator.StringToHash("mouthState");
 
-    [Foldout("HeadMovement")]
+    [Header("Movement")]
     [SerializeField] float headPullForce = 8f;
-    [Foldout("HeadMovement")]
     [SerializeField] float headPullVelocity = 15f;
-    [Foldout("HeadMovement")]
     [Range(0, .3f)][SerializeField] float movementSmoothing = 0.05f;
-    [Foldout("HeadMovement")]
     [Range(0, 8f)][SerializeField] float maxNeckLength = 3.4f;
-    [Foldout("HeadMovement")]
     [SerializeField] LayerMask grabbableLayers;
-    [Foldout("HeadMovement")]
     [SerializeField] float mouseChatterThreshold = 0.5f;
+    [SerializeField] float extraWallCheckRadius = 0.2f;
 
-    [Foldout("PotMovement")]
+    [Header("Swinging")]
     [SerializeField] float minMousePotSwingDist = 0.5f;
-    [Foldout("PotMovement")]
     [SerializeField] float mousePotSwingRadius = 2f;
-    [Foldout("PotMovement")]
     [SerializeField] float maxPotSwingForce = 75f;
 
-    [Foldout("Misc")]
-    [SerializeField] float extraWallCheckRadius = 0.2f;
-    [Foldout("Misc")]
-    [Range(-0.5f, 0)][SerializeField] float comOffset = -0.2f;
-    [Foldout("Misc")]
+    [Header("Not Stuck")]
     [SerializeField] float targetFreeDist = 1f;
-    [Foldout("Misc")]
     [SerializeField] float freeForceP = 1;
-    [Foldout("Misc")]
     [SerializeField] float freeForceD = 0;
-    [Foldout("Misc")]
     [SerializeField] float freeAngleP = 1;
-    [Foldout("Misc")]
     [SerializeField] float freeAngleD = 0;
-    [Foldout("Misc")]
     [SerializeField] Vector2 flowerpotVBias;
 
+    [Header("References")]
     public Transform neck;
     public Rigidbody2D flowerpot;
     public Rigidbody2D tongue;
@@ -79,6 +65,7 @@ public class PlayerMovement : MonoBehaviour
 
     [HideInInspector] public Rigidbody2D stuckTo = null;
     [HideInInspector] public Collider2D stuckToCollider = null;
+    [Header("States")]
     [ReadOnly] public bool stuck = false;
     [ReadOnly] public bool mouthFull = false;
     [ReadOnly] public bool retractingTongue = false;
