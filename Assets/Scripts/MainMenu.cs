ed8a031959cbce7962af42e7b273065004cda7db
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2023-03-26 15:40:09 -0400

    added title screen, chomp noise

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] public string firstLevelFilepath = "";
+
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevelFilepath);
+    }
+
+}
