433f6ece2b7bb67e9aec6279743d455755021245
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-04-06 16:30:02 -0400

    Interfaces class and bool acceptor

---
+++ Assets/Scripts/Interfaces/IBoolAcceptor.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBoolAcceptor
+{
+    void TakeBool(bool value);
+}
---
+++ Assets/Scripts/Interfaces/Toggleable.cs
@@ -0,0 +1,49 @@
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class Toggleable: MonoBehaviour, IBoolAcceptor
+{
+    [OnValueChanged("SetOnEditor")][SerializeField] protected bool on = false;
+    [SerializeField] protected bool invertReceivedBool = false;
+
+    private void Start()
+    {
+        SetOn(on);
+    }
+
+    public virtual void SetOn(bool state)
+    {
+        on = state;
+    }
+
+    public void TurnOn()
+    {
+        SetOn(true);
+    }
+
+    public void TurnOff()
+    {
+        SetOn(false);
+    }
+
+    public void Toggle()
+    {
+        SetOn(!on);
+    }
+
+    public void TakeBool(bool value)
+    {
+        // We XOR the received value and whether we invert it to get the new on value
+        SetOn(value ^ invertReceivedBool);
+    }
+
+    public bool IsOn() => on;
+
+    protected virtual void SetOnEditor()
+    {
+        SetOn(on);
+    }
+}
