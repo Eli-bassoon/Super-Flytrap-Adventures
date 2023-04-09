e775b43dbb0b8e34a5f022099dab6b506c623d8c
clytieq <113843835+clytieq@users.noreply.github.com>
2023-04-09 14:26:48 -0400

    999+ errors

---
+++ Assets/Scripts/Player/DamageHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+    [SerializeField] int fullHealth = 100;
+    [SerializeField] int currHealth; // = fullHealth;
+    [SerializeField] GameObject checkpointPrefab; // drag prefab here 
+    [SerializeField] Transform checkpointLatest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = fullHealth;
+        // spawn prefab & save its transform
+        checkpointLatest = Instantiate(checkpointPrefab, transform.position, Quaternion.identity).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        currHealth -= dmg;
+        if (currHealth < 0)
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        GetComponent<Rigidbody2D>().position = checkpointLatest.position;
+    }
+}
