1546f1f4ae46056fb970c612028f12a525865235
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-04-12 04:04:03 -0400

    Add component tool panel package

---
+++ Assets/Packages/Component Tool Panel/Editor/Scripts/TransformInspector.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ComponentToolPanel
+{
+	[CustomEditor(typeof(Transform), true)]
+	[CanEditMultipleObjects]
+	public class TransformInspector : ExtendedEditor
+	{
+		private static Texture transformIcon;
+		private Transform transform;
+		protected override string DefaultEditorTypeName => "UnityEditor.TransformInspector, UnityEditor";
+		protected override Type InspectedType => typeof(Transform);
+
+		protected override void OnEnableInternal()
+		{
+			transform = target as Transform;
+			if (transformIcon == null)
+				transformIcon = EditorGUIUtility.ObjectContent(transform, typeof(Transform)).image;
+		}
+
+		protected override void OnCustomInspectorGUI()
+		{
+			DrawLocalTransform();
+			DrawWorldTransform();
+
+			if (targets.Length > 1) return;
+			if (transform.parent == null && transform.childCount <= 0) return;
+
+			DrawTransformInfoBox();
+		}
+
+		private void DrawWorldTransform()
+		{
+			EditorGUILayout.Space();
+			EditorGUILayout.LabelField("World Space", EditorStyles.boldLabel);
+
+			GUI.enabled = false;
+			ShowWorldTransform();
+			GUI.enabled = true;
+		}
+
+		private void DrawLocalTransform()
+		{
+			EditorGUILayout.LabelField("Local Space", EditorStyles.boldLabel);
+			defaultEditor.OnInspectorGUI();
+		}
+
+		private void ShowWorldTransform()
+		{
+			var worldPosition = transform.position;
+			var worldRotation = transform.rotation;
+			var worldScale = transform.lossyScale;
+
+			EditorGUILayout.Vector3Field("Position", worldPosition);
+			EditorGUILayout.Vector3Field("Rotation", worldRotation.eulerAngles);
+			EditorGUILayout.Vector3Field("Scale", worldScale);
+		}
+
+		private void DrawTransformInfoBox()
+		{
+			var labelWidth = EditorGUIUtility.labelWidth;
+			var fieldWidth = EditorGUIUtility.fieldWidth;
+			var fontSize = EditorStyles.label.fontSize;
+			EditorGUIUtility.labelWidth = 1;
+			EditorGUIUtility.fieldWidth = 1;
+			EditorStyles.label.fontSize = 9;
+
+			EditorGUILayout.Space();
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+			ShowTransformInfoBox();
+			EditorGUILayout.EndVertical();
+
+			EditorGUIUtility.labelWidth = labelWidth;
+			EditorGUIUtility.fieldWidth = fieldWidth;
+			EditorStyles.label.fontSize = fontSize;
+		}
+
+		private void ShowTransformInfoBox()
+		{
+			var text = string.Empty;
+			if (transform.parent != null)
+			{
+				EditorGUILayout.BeginHorizontal();
+				ShowParentAndRootTransform();
+				EditorGUILayout.EndHorizontal();
+
+				text = $"Sibling Index: {transform.GetSiblingIndex()}\t";
+			}
+
+			ShowSiblingAndChildrenCount(text);
+			UnParent();
+		}
+
+		private void ShowParentAndRootTransform()
+		{
+			var currentEvent = Event.current;
+			var root = transform.root;
+			EditorGUILayout.LabelField(new GUIContent($"Root: {root.name}", transformIcon));
+			SelectGameObject(currentEvent, root);
+
+			var parent = transform.parent;
+			EditorGUILayout.LabelField(new GUIContent("Parent: " + parent.name, transformIcon));
+			SelectGameObject(currentEvent, parent);
+		}
+
+		private void SelectGameObject(Event e, Transform selectedTransform)
+		{
+			var lastRect = GUILayoutUtility.GetLastRect();
+			if (e.type != EventType.MouseDown || e.button != 0 || !lastRect.Contains(e.mousePosition)) return;
+
+			if (e.clickCount == 2)
+			{
+				Selection.activeTransform = selectedTransform;
+			}
+			else
+			{
+				EditorGUIUtility.PingObject(selectedTransform);
+			}
+		}
+
+		private void ShowSiblingAndChildrenCount(string text)
+		{
+			if (transform.childCount > 0)
+				text += $"Child Count: {transform.childCount}";
+
+			EditorGUILayout.LabelField(text);
+		}
+
+		private void UnParent()
+		{
+			if (transform.parent is null || !GUILayout.Button("UnParent", EditorStyles.miniButton)) return;
+			Undo.SetTransformParent(transform, null, "UnParent");
+			transform.SetParent(null);
+		}
+	}
+}
---
+++ Assets/Packages/Component Tool Panel/Editor/Scripts/Utils.cs
@@ -0,0 +1,132 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace ComponentToolPanel
+{
+	public static class Icons
+	{
+		private const string InfoIconName = "console.infoicon.sml";
+		private const string NonFavoriteIconName = "Favorite";
+		private const string FavoriteIconName = "Favorite Icon";
+		private const string CloseIconName = "TL Close button act";
+		private const string DocumentIconName = "UnityEditor.ConsoleWindow";
+		private const string FolderIconName = "Project";
+		private const string ResetIconName = "playLoopOff";
+		private const string EditIconName = "ViewToolZoom";
+		private const string WarningIconName = "console.warnicon.sml";
+		private static Texture2D infoIcon;
+		private static Texture2D nonFavoriteIcon;
+		private static Texture2D favoriteIcon;
+		private static Texture2D closeIcon;
+		private static Texture2D documentIcon;
+		private static Texture2D folderIcon;
+		private static Texture2D resetIcon;
+		private static Texture2D editIcon;
+		private static Texture2D warningIcon;
+
+		public static Texture2D InfoIcon
+		{
+			get
+			{
+				if (infoIcon == null)
+					infoIcon = FindTexture(InfoIconName);
+				return infoIcon;
+			}
+		}
+
+		public static Texture2D NonFavoriteIcon
+		{
+			get
+			{
+				if (nonFavoriteIcon == null)
+					nonFavoriteIcon = FindTexture(NonFavoriteIconName);
+				return nonFavoriteIcon;
+			}
+		}
+
+		public static Texture2D FavoriteIcon
+		{
+			get
+			{
+				if (favoriteIcon == null)
+					favoriteIcon = FindTexture(FavoriteIconName);
+				return favoriteIcon;
+			}
+		}
+
+		public static Texture2D CloseIcon
+		{
+			get
+			{
+				if (closeIcon == null)
+					closeIcon = FindTexture(CloseIconName);
+				return closeIcon;
+			}
+		}
+
+		public static Texture2D DocumentIcon
+		{
+			get
+			{
+				if (documentIcon == null)
+					documentIcon = FindTexture(DocumentIconName);
+				return documentIcon;
+			}
+		}
+
+		public static Texture2D FolderIcon
+		{
+			get
+			{
+				if (folderIcon == null)
+					folderIcon = FindTexture(FolderIconName);
+				return folderIcon;
+			}
+		}
+
+		public static Texture2D ResetIcon
+		{
+			get
+			{
+				if (resetIcon == null)
+					resetIcon = FindTexture(ResetIconName);
+				return resetIcon;
+			}
+		}
+
+		public static Texture2D EditIcon
+		{
+			get
+			{
+				if (editIcon == null)
+					editIcon = FindTexture(EditIconName);
+				return editIcon;
+			}
+		}
+
+		public static Texture2D WarningIcon
+		{
+			get
+			{
+				if (warningIcon == null)
+					warningIcon = FindTexture(WarningIconName);
+				return warningIcon;
+			}
+		}
+
+		private static Texture2D FindTexture(string iconName)
+		{
+			Texture2D texture2D = null;
+			texture2D = EditorGUIUtility.IconContent(iconName).image as Texture2D;
+
+			if (texture2D == null)
+				texture2D = Resources.FindObjectsOfTypeAll<Texture2D>().FirstOrDefault(x => x.name == iconName);
+
+			if (texture2D == null)
+				texture2D = EditorGUIUtility.FindTexture(iconName);
+
+			return texture2D;
+		}
+	}
+}
