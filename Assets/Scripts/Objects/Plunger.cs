8c732aad757c41e4b44a099703356ef8156b4896
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-04-12 01:00:57 -0400

    Functional plunger

--- Assets/Scripts/Objects/Plunger.cs
+++ Assets/Scripts/Objects/Plunger.cs
@@ -1,21 +1,65 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Plunger : MonoBehaviour
 {
     [SerializeField] Rigidbody2D handle;
     [SerializeField] float depressedThreshold = 0.9f;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] subscriberObjects;
+    List<IBoolAcceptor> subscribers;
+    public UnityEvent PushedInEvent;
+    public UnityEvent PulledOutEvent;
+
+    Rigidbody2D rb;
+    float activateDist;
+
+    [ReadOnly] public bool activated = false;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        activateDist = depressedThreshold * GetComponent<SliderJoint2D>().limits.max;
+
+        subscribers = new List<IBoolAcceptor>(subscriberObjects.Length);
+        foreach (var sub in subscriberObjects)
+        {
+            subscribers.Add(sub.GetComponent<IBoolAcceptor>());
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector2.Distance(rb.position, handle.position) >= activateDist)
+        {
+            if (!activated)
+            {
+                activated = true;
+                PushedInEvent.Invoke();
+                foreach (var sub in subscribers)
+                {
+                    sub.TakeBool(activated);
+                }
+            }
+        }
+        else
+        {
+            if (activated)
+            {
+                activated = false;
+                PulledOutEvent.Invoke();
+                foreach (var sub in subscribers)
+                {
+                    sub.TakeBool(activated);
+                }
+            }
+        }
     }
 }
