ef5978408622ff7b21bc10fd1fa7743c56d6bdcd
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-04-22 00:06:35 -0400

    Make android builds work

--- Assets/Scripts/Objects/Crank.cs
+++ Assets/Scripts/Objects/Crank.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 using UnityEngine.UIElements;
 
 public class Crank : MonoBehaviour
