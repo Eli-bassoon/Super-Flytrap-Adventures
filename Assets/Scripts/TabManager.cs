48477bf5c9ec2d8d72b544ee21a31ed1cb135104
clytieq <113843835+clytieq@users.noreply.github.com>
2023-04-23 16:04:44 -0400

    start making tabs in pause menu
    see comments in tabmanager

---
+++ Assets/Scripts/TabManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabManager : MonoBehaviour
+{
+    [SerializeField] GameObject audio; // something like this
+    [SerializeField] GameObject gameplay;
+    // to do: disable audio/gameplay depending on which button was last pressed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void showAudio()
+    {
+        // enable game objects, either thru 
+    }
+
+    void hideAudio()
+    {
+        // disable game objects
+    }
+
+    // repeat for game tab
+    // also implement speedrunner stuff
+}
