becb993e04041937d1d8e83bea95988848c0e96a
clytieq <113843835+clytieq@users.noreply.github.com>
2023-04-23 21:54:50 -0400

    audio & gameplay tabs interface in pause menu
    gameplay toggles are ugly and don't do anything yet

--- Assets/Scripts/TabManager.cs
+++ Assets/Scripts/TabManager.cs
@@ -1,35 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabManager : MonoBehaviour
 {
-    [SerializeField] GameObject audio; // something like this
-    [SerializeField] GameObject gameplay;
+    [SerializeField] GameObject audioSettings;
+    [SerializeField] GameObject gameplaySettings;
+    [SerializeField] Button audioTabButton;
+    [SerializeField] Button gameplayTabButton;
+    [SerializeField] Color tabColor;
+
+    TextMeshProUGUI audioButtonText;
+    TextMeshProUGUI gameplayButtonText;
+
+    private bool viewingAudio;
     // to do: disable audio/gameplay depending on which button was last pressed
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        viewingAudio = true;
+        audioSettings.SetActive(true);
+        gameplaySettings.SetActive(false);
+
+        audioButtonText = audioTabButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        gameplayButtonText = gameplayTabButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (viewingAudio)
+        {
+            audioButtonText.color = tabColor;
+            gameplayButtonText.color = Color.gray; 
+        }
+        else
+        {
+            audioButtonText.color = Color.gray;
+            gameplayButtonText.color = tabColor;
+        }
+    }
+
+    public void onClickAudioTab()
+    {
+        if (viewingAudio) return;
+        toggleTabs();
+    }
+
+    public void onClickGameplayTab()
+    {
+        if (!viewingAudio) return;
+        toggleTabs();
+    }
+
+    void toggleTabs()
+    {
+        viewingAudio = !audioSettings.activeSelf;
+        audioSettings.SetActive(viewingAudio);
+        gameplaySettings.SetActive(!viewingAudio);
         
+        //audioTabButton.transform.GetChild(0).GetComponent<TextMeshPro>
     }
-
-    void showAudio()
-    {
-        // enable game objects, either thru 
-    }
-
-    void hideAudio()
-    {
-        // disable game objects
-    }
-
-    // repeat for game tab
-    // also implement speedrunner stuff
 }
