243781ab58e1321ff259ecbd84709405724b9b03
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-04-28 05:57:09 -0400

    Improve meme hallway zooming (not by much)

--- Assets/Scripts/TriggerCamera.cs
+++ Assets/Scripts/TriggerCamera.cs
@@ -6,32 +6,33 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class TriggerCamera : MonoBehaviour
 {
-    [SerializeField] public Vector2Int cameraBound;
+    [SerializeField] Vector2Int cameraBound;
+    [SerializeField] float zoomTime = 3f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        IEnumerator Zoom()
-        {
-            var cam = Camera.main.GetComponent<PixelPerfectCamera>();
-            while (cam.refResolutionX != cameraBound.x || cam.refResolutionY != cameraBound.y)
-            {
-                if (cameraBound.x > cam.refResolutionX) 
-                {
-                    cam.refResolutionX = (int)Mathf.Lerp(cam.refResolutionX, cam.refResolutionX + 1, 1);
-                    cam.refResolutionY = (int)Mathf.Lerp(cam.refResolutionY, cam.refResolutionY + 1, 1);
-                }
-                else
-                {
-                    cam.refResolutionX = (int)Mathf.Lerp(cam.refResolutionX, cam.refResolutionX - 1, 1);
-                    cam.refResolutionY = (int)Mathf.Lerp(cam.refResolutionY, cam.refResolutionY - 1, 1);
-                }
-                yield return null;
-            }
-        }
-
         // Only save checkpoint when the flowerpot hits it
         if (collision.gameObject.CompareTag("Player") && collision.TryGetComponent(out Flowerpot _))
         {
             StartCoroutine(Zoom());
+        }
+    }
+
+    IEnumerator Zoom()
+    {
+        var cam = Camera.main.GetComponent<PixelPerfectCamera>();
+
+        Vector2Int startRes = new Vector2Int(cam.refResolutionX, cam.refResolutionY);
+
+        float t;
+        float startTime = Time.time;
+
+        while ((t = (Time.time - startTime) / zoomTime) < zoomTime)
+        {
+            cam.refResolutionX = (int)Mathf.Lerp(startRes.x, cameraBound.x, t);
+            cam.refResolutionY = (int)Mathf.Lerp(startRes.y, cameraBound.y, t);
+
+            yield return new WaitForEndOfFrame();
         }
     }
 }
