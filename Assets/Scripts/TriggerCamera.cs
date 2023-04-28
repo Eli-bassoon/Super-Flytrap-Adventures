5e602124c438984db2d45fa05f92fffc9daddf12
Nate-TheBeefCloud <naoswell@hotmail.com>
2023-04-27 21:41:05 -0400

    added camera zoom out in secret, very scuffed, does not work to zoom back in

--- Assets/Scripts/TriggerCamera.cs
+++ Assets/Scripts/TriggerCamera.cs
@@ -9,12 +9,29 @@ public class TriggerCamera : MonoBehaviour
     [SerializeField] public Vector2Int cameraBound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        IEnumerator Zoom()
+        {
+            var cam = Camera.main.GetComponent<PixelPerfectCamera>();
+            while (cam.refResolutionX != cameraBound.x || cam.refResolutionY != cameraBound.y)
+            {
+                if (cameraBound.x > cam.refResolutionX) 
+                {
+                    cam.refResolutionX = (int)Mathf.Lerp(cam.refResolutionX, cam.refResolutionX + 1, 1);
+                    cam.refResolutionY = (int)Mathf.Lerp(cam.refResolutionY, cam.refResolutionY + 1, 1);
+                }
+                else
+                {
+                    cam.refResolutionX = (int)Mathf.Lerp(cam.refResolutionX, cam.refResolutionX - 1, 1);
+                    cam.refResolutionY = (int)Mathf.Lerp(cam.refResolutionY, cam.refResolutionY - 1, 1);
+                }
+                yield return null;
+            }
+        }
+
         // Only save checkpoint when the flowerpot hits it
         if (collision.gameObject.CompareTag("Player") && collision.TryGetComponent(out Flowerpot _))
         {
-            var cam = Camera.main.GetComponent<PixelPerfectCamera>();
-            cam.refResolutionX = cameraBound.x;
-            cam.refResolutionY = cameraBound.y;
+            StartCoroutine(Zoom());
         }
     }
 }
