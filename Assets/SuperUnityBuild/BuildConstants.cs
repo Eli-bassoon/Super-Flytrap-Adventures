e5abb2a652ca7df8ee0120909cfc66d3f37bc4bc
elijfox <61474669+Eli-bassoon@users.noreply.github.com>
2023-09-17 14:36:09 -0400

    Build scripts and itch.io upload automation

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,41 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Release,
+    }
+
+    public enum Platform
+    {
+        None,
+        Android,
+        PC,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Android,
+        Windows_x64,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly DateTime buildDate = new DateTime(638305025870446407);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Release;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.None;
+}
+
