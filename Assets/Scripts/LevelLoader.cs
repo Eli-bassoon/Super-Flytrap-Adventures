59d3f1f14cb7419a218f31689a7feea6b9fe4fc6
elijfox <61474669+Eli-bassoon@users.noreply.github.com>
2023-11-20 21:08:57 -0500

    Restructure levels to be one scene

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,19 +8,10 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// A utility class to load multi-scene levels
+// A utility class to load levels
 public class LevelLoader : MonoBehaviour
 {
-    static List<List<string>> levelScenes = new List<List<string>>
-    {
-        // Level 1
-        new List<string> {"Intro", "Hub", "Hanging Pots", "Toxic" },
-        
-        // Level 2
-        new List<string> {"Hub", "Light1", "Light2", "End" },
-    };
-
-    const string SCENE_PATH = "Assets/Scenes/Levels/Level";
+    const string SCENE_PATH = "Scenes/Levels/Level";
 
     public static bool levelLoaded = false;
 
@@ -35,22 +26,20 @@ public class LevelLoader : MonoBehaviour
         // Start loading the level
         levelLoaded = false;
 
-        List<string> thisLevelScenes = levelScenes[level - 1];
-
         // Load the main scene of the level
-        string firstScenePath = JoinScenePath(level, thisLevelScenes[0]);
+        string scenePath = SCENE_PATH + " " + level;
 
         // Editor only
 #if UNITY_EDITOR
         if (Application.isPlaying)
         {
-            SceneManager.LoadScene(firstScenePath);
+            SceneManager.LoadScene(scenePath);
             GameManager.GM.SetLevel(level - 1);
         }
         else
         {
             EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-            EditorSceneManager.OpenScene(firstScenePath);
+            EditorSceneManager.OpenScene(scenePath);
 
             // This is black magic but it makes the level in frame correctly
             GameObject tm = GameObject.Find("Tiles");
@@ -71,55 +60,12 @@ public class LevelLoader : MonoBehaviour
         // Game mode
 #else
         // Load the scene in the game
-        SceneManager.LoadScene(firstScenePath);
+        SceneManager.LoadScene(scenePath);
 
         // Change the level the game thinks it's on
         GameManager.GM.SetLevel(level - 1);
 #endif
 
-        // Load the extra scenes of the level
-        foreach (string sceneName in thisLevelScenes.Skip(1))
-        {
-            string scenePath = JoinScenePath(level, sceneName);
-
-#if UNITY_EDITOR
-            if (Application.isPlaying)
-            {
-                SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Additive);
-            }
-            else
-            {
-                EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
-            }
-#else
-            SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Additive);
-#endif
-        }
         levelLoaded = true;
     }
-
-    static string JoinScenePath(int level, string sceneName)
-    {
-        return SCENE_PATH + level + "/" + sceneName + ".unity";
-    }
-
-    // Unity editor menu options
-#if UNITY_EDITOR
-
-    const string MULTI_SCENE_MENU = "File/Load Multi Scene Level/";
-    const int START_PRIORITY = 150;
-
-    [MenuItem(MULTI_SCENE_MENU + "Level 1", priority = START_PRIORITY + 1)]
-    static void LoadLevel1()
-    {
-        LoadLevel(1);
-    }
-
-    [MenuItem(MULTI_SCENE_MENU + "Level 2", priority = START_PRIORITY + 2)]
-    static void LoadLevel2()
-    {
-        LoadLevel(2);
-    }
-
-#endif
 }
